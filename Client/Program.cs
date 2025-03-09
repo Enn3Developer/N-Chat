00e724d7aca2a06a5ee5cb38a7d5ab61fc7f1921
Enn3Developer <arturpeshko39@gmail.com>
2025-03-09 13:00:36 +0100

    changed Program.cs

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Client;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+    }
+}
