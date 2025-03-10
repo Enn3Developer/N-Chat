f5f627a72ad33493aa273c695fa2d6bdf4a62130
Enn3Developer <arturpeshko39@gmail.com>
2025-03-10 13:03:05 +0100

    working prototype

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -2,13 +2,15 @@ mod validation;
 
 use crate::validation::{validate_message, validate_name};
 use spacetimedb::{reducer, table, Identity, ReducerContext, Table, Timestamp};
+use std::ops::Add;
 
 /// Defines a user
 #[table(name = user, public)]
 pub struct User {
     #[primary_key]
     id: Identity,
-    name: Option<String>,
+    #[unique]
+    name: String,
     online: bool,
 }
 
@@ -25,6 +27,7 @@ pub struct Channel {
     #[primary_key]
     #[auto_inc]
     id: i128,
+    #[unique]
     name: String,
 }
 
@@ -41,7 +44,7 @@ pub struct Message {
     #[primary_key]
     #[auto_inc]
     id: i128,
-    sender: Identity,
+    sender: String,
     channel_id: i128,
     sent: Timestamp,
     text: String,
@@ -56,11 +59,6 @@ pub fn client_connected(ctx: &ReducerContext) {
         });
     } else {
         log::info!("New user connected");
-        ctx.db.user().insert(User {
-            id: ctx.sender,
-            online: true,
-            name: None,
-        });
     }
 }
 
@@ -78,27 +76,29 @@ pub fn client_disconnected(ctx: &ReducerContext) {
 
 #[reducer]
 pub fn set_name(ctx: &ReducerContext, name: String) -> Result<(), String> {
-    let user = ctx
-        .db
-        .user()
-        .id()
-        .find(ctx.sender)
-        .ok_or("User is not registered")?;
-
     if !validate_name(&name) {
         return Err("Name isn't valid".into());
     }
 
-    ctx.db.user().id().update(User {
-        name: Some(name),
-        ..user
-    });
+    if ctx.db.user().name().find(&name).is_some() {
+        return Err("Name not available".into());
+    }
+
+    if let Some(user) = ctx.db.user().id().find(ctx.sender) {
+        ctx.db.user().id().update(User { name, ..user });
+    } else {
+        ctx.db.user().insert(User {
+            id: ctx.sender,
+            name,
+            online: true,
+        });
+    }
 
     Ok(())
 }
 
 #[reducer]
-pub fn send_message(ctx: &ReducerContext, text: String, channel: i128) -> Result<(), String> {
+pub fn send_message(ctx: &ReducerContext, text: String, channel: String) -> Result<(), String> {
     let user = ctx
         .db
         .user()
@@ -106,13 +106,12 @@ pub fn send_message(ctx: &ReducerContext, text: String, channel: i128) -> Result
         .find(ctx.sender)
         .ok_or("User is not registered")?;
 
-    if user.name.is_none() {
-        return Err("User doesn't have a name".into());
-    }
-
-    if ctx.db.channel().id().find(channel).is_none() {
-        return Err("Channel doesn't exist".into());
-    }
+    let channel = ctx
+        .db
+        .channel()
+        .name()
+        .find(channel)
+        .ok_or("No channel found")?;
 
     if !validate_message(&text) {
         return Err("Message content isn't valid".into());
@@ -120,8 +119,8 @@ pub fn send_message(ctx: &ReducerContext, text: String, channel: i128) -> Result
 
     ctx.db.message().insert(Message {
         id: 0,
-        sender: ctx.sender,
-        channel_id: channel,
+        sender: user.name,
+        channel_id: channel.id,
         sent: ctx.timestamp,
         text,
     });
@@ -138,12 +137,12 @@ pub fn create_channel(ctx: &ReducerContext, channel_name: String) -> Result<(), 
         .find(ctx.sender)
         .ok_or("User is not registered")?;
 
-    if user.name.is_none() {
-        return Err("User doesn't have a name".into());
-    }
-
     if !validate_name(&channel_name) {
         return Err("Channel name isn't valid".into());
+    }
+
+    if ctx.db.channel().name().find(&channel_name).is_some() {
+        return Err("A channel already exists with this name".into());
     }
 
     let channel = ctx.db.channel().insert(Channel {
@@ -152,7 +151,7 @@ pub fn create_channel(ctx: &ReducerContext, channel_name: String) -> Result<(), 
     });
 
     ctx.db.member().insert(Member {
-        user_id: ctx.sender,
+        user_id: user.id,
         channel_id: channel.id,
     });
 
@@ -160,41 +159,59 @@ pub fn create_channel(ctx: &ReducerContext, channel_name: String) -> Result<(), 
 }
 
 #[reducer]
-pub fn add_user(ctx: &ReducerContext, channel_id: i128, user_id: Identity) -> Result<(), String> {
-    if ctx.db.channel().id().find(channel_id).is_none() {
+pub fn add_user(ctx: &ReducerContext, channel: String, user_name: String) -> Result<(), String> {
+    let chan: Channel;
+    if let Some(c) = ctx.db.channel().name().find(&channel) {
+        chan = c;
+    } else {
+        log::info!("channel: {channel}");
+        log::info!(
+            "channels: {:?}",
+            ctx.db
+                .channel()
+                .iter()
+                .fold(String::new(), |str, channel| str.add(&channel.name))
+        );
         return Err("No channel found".into());
     }
 
-    if ctx.db.user().id().find(user_id).is_none() {
-        return Err("No user found".into());
-    }
+    let user = ctx
+        .db
+        .user()
+        .name()
+        .find(user_name)
+        .ok_or("No user found")?;
 
     ctx.db.member().insert(Member {
-        user_id,
-        channel_id,
+        user_id: user.id,
+        channel_id: chan.id,
     });
 
     Ok(())
 }
 
 #[reducer]
-pub fn add_friend(ctx: &ReducerContext, user_id: Identity) -> Result<(), String> {
-    if ctx.db.user().id().find(ctx.sender).is_none() || ctx.db.user().id().find(user_id).is_none() {
-        return Err("No user found".into());
-    }
+pub fn add_friend(ctx: &ReducerContext, user_name: String) -> Result<(), String> {
+    let user_a = ctx.db.user().id().find(ctx.sender).ok_or("No user found")?;
+    let user_b = ctx
+        .db
+        .user()
+        .name()
+        .find(user_name)
+        .ok_or("No user found")?;
 
     // This sucks, it'd be better to compute an id starting from the two user ids
     for friend in ctx.db.friend().iter() {
-        if (friend.user_a == ctx.sender && friend.user_b == user_id)
-            || (friend.user_b == ctx.sender && friend.user_a == user_id)
+        if (friend.user_a == user_a.id && friend.user_b == user_b.id)
+            || (friend.user_b == user_a.id && friend.user_a == user_b.id)
         {
             return Err("Already a friend".into());
         }
     }
 
     ctx.db.friend().insert(Friend {
-        user_a: ctx.sender,
-        user_b: user_id,
+        user_a: user_a.id,
+        user_b: user_b.id,
     });
 
     Ok(())
