7cf01c916f4aaaafac7b8218002213ba9fbaf746
Enn3Developer <arturpeshko39@gmail.com>
2025-03-10 23:17:59 +0100

    optimized friend handling

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -2,7 +2,7 @@ mod validation;
 
 use crate::validation::{validate_message, validate_name};
 use spacetimedb::{reducer, table, Identity, ReducerContext, Table, Timestamp};
-use std::ops::Add;
+use std::hash::{DefaultHasher, Hash, Hasher};
 
 /// Defines a user
 #[table(name = user, public)]
@@ -17,6 +17,16 @@ pub struct User {
 /// Defines a friendship relation between two users
 #[table(name = friend, public)]
 pub struct Friend {
+    #[unique]
+    hash: u64,
+    user_a: Identity,
+    user_b: Identity,
+}
+
+#[table(name = friend_request, public)]
+pub struct FriendRequest {
+    #[unique]
+    hash: u64,
     user_a: Identity,
     user_b: Identity,
 }
@@ -52,6 +62,7 @@ pub struct Message {
 
 #[reducer(client_connected)]
 pub fn client_connected(ctx: &ReducerContext) {
+    // update the user if it exists
     if let Some(user) = ctx.db.user().id().find(ctx.sender) {
         ctx.db.user().id().update(User {
             online: true,
@@ -64,6 +75,7 @@ pub fn client_connected(ctx: &ReducerContext) {
 
 #[reducer(client_disconnected)]
 pub fn client_disconnected(ctx: &ReducerContext) {
+    // update the user if it exists
     if let Some(user) = ctx.db.user().id().find(ctx.sender) {
         ctx.db.user().id().update(User {
             online: false,
@@ -76,20 +88,24 @@ pub fn client_disconnected(ctx: &ReducerContext) {
 
 #[reducer]
 pub fn set_name(ctx: &ReducerContext, name: String) -> Result<(), String> {
+    // validate the name
     if !validate_name(&name) {
         return Err("Name isn't valid".into());
     }
 
+    // check if the name is unique
     if ctx.db.user().name().find(&name).is_some() {
         return Err("Name not available".into());
     }
 
+    // update or add the user
     if let Some(user) = ctx.db.user().id().find(ctx.sender) {
         ctx.db.user().id().update(User { name, ..user });
     } else {
         ctx.db.user().insert(User {
             id: ctx.sender,
             name,
+            // we set it to true because we assume the user is connected before it can use reducers
             online: true,
         });
     }
@@ -99,6 +115,7 @@ pub fn set_name(ctx: &ReducerContext, name: String) -> Result<(), String> {
 
 #[reducer]
 pub fn send_message(ctx: &ReducerContext, text: String, channel: String) -> Result<(), String> {
+    // get the user
     let user = ctx
         .db
         .user()
@@ -106,6 +123,7 @@ pub fn send_message(ctx: &ReducerContext, text: String, channel: String) -> Resu
         .find(ctx.sender)
         .ok_or("User is not registered")?;
 
+    // get the channel
     let channel = ctx
         .db
         .channel()
@@ -113,11 +131,15 @@ pub fn send_message(ctx: &ReducerContext, text: String, channel: String) -> Resu
         .find(channel)
         .ok_or("No channel found")?;
 
+    // validate the message
     if !validate_message(&text) {
         return Err("Message content isn't valid".into());
     }
 
+    // add the message
     ctx.db.message().insert(Message {
+        // it is 0 because `id` is `auto_inc` so it is changed before committing the change
+        // to the database with the new id value
         id: 0,
         sender: user.name,
         channel_id: channel.id,
@@ -130,6 +152,7 @@ pub fn send_message(ctx: &ReducerContext, text: String, channel: String) -> Resu
 
 #[reducer]
 pub fn create_channel(ctx: &ReducerContext, channel_name: String) -> Result<(), String> {
+    // get the user
     let user = ctx
         .db
         .user()
@@ -137,19 +160,23 @@ pub fn create_channel(ctx: &ReducerContext, channel_name: String) -> Result<(), 
         .find(ctx.sender)
         .ok_or("User is not registered")?;
 
+    // check the channel name
     if !validate_name(&channel_name) {
         return Err("Channel name isn't valid".into());
     }
 
+    // check if the channel name is unique
     if ctx.db.channel().name().find(&channel_name).is_some() {
         return Err("A channel already exists with this name".into());
     }
 
+    // create the channel
     let channel = ctx.db.channel().insert(Channel {
         id: 0,
         name: channel_name,
     });
 
+    // add the user as a member of the channel
     ctx.db.member().insert(Member {
         user_id: user.id,
         channel_id: channel.id,
@@ -160,21 +187,15 @@ pub fn create_channel(ctx: &ReducerContext, channel_name: String) -> Result<(), 
 
 #[reducer]
 pub fn add_user(ctx: &ReducerContext, channel: String, user_name: String) -> Result<(), String> {
-    let chan: Channel;
-    if let Some(c) = ctx.db.channel().name().find(&channel) {
-        chan = c;
-    } else {
-        log::info!("channel: {channel}");
-        log::info!(
-            "channels: {:?}",
-            ctx.db
-                .channel()
-                .iter()
-                .fold(String::new(), |str, channel| str.add(&channel.name))
-        );
-        return Err("No channel found".into());
-    }
+    // get the channel
+    let channel = ctx
+        .db
+        .channel()
+        .name()
+        .find(&channel)
+        .ok_or("No channel found")?;
 
+    // get the user
     let user = ctx
         .db
         .user()
@@ -182,9 +203,10 @@ pub fn add_user(ctx: &ReducerContext, channel: String, user_name: String) -> Res
         .find(user_name)
         .ok_or("No user found")?;
 
+    // add the user as a member of the channel
     ctx.db.member().insert(Member {
         user_id: user.id,
-        channel_id: chan.id,
+        channel_id: channel.id,
     });
 
     Ok(())
@@ -192,6 +214,7 @@ pub fn add_user(ctx: &ReducerContext, channel: String, user_name: String) -> Res
 
 #[reducer]
 pub fn add_friend(ctx: &ReducerContext, user_name: String) -> Result<(), String> {
+    // get users
     let user_a = ctx.db.user().id().find(ctx.sender).ok_or("No user found")?;
     let user_b = ctx
         .db
@@ -200,16 +223,90 @@ pub fn add_friend(ctx: &ReducerContext, user_name: String) -> Result<(), String>
         .find(user_name)
         .ok_or("No user found")?;
 
-    // This sucks, it'd be better to compute an id starting from the two user ids
-    for friend in ctx.db.friend().iter() {
-        if (friend.user_a == user_a.id && friend.user_b == user_b.id)
-            || (friend.user_b == user_a.id && friend.user_a == user_b.id)
-        {
-            return Err("Already a friend".into());
-        }
+    // we compute the hashes of the user ids and use that has the unique id
+    // this way we avoid to iterate through each row and the database can optimize its access
+    let mut hasher = DefaultHasher::new();
+    ctx.sender.hash(&mut hasher);
+    user_b.id.hash(&mut hasher);
+    let hash_a = hasher.finish();
+
+    // we compute two hashes because the ordering changes the hash
+    let mut hasher = DefaultHasher::new();
+    user_b.id.hash(&mut hasher);
+    ctx.sender.hash(&mut hasher);
+    let hash_b = hasher.finish();
+
+    // check if the user are already friends with each other
+    if ctx.db.friend().hash().find(hash_a).is_some()
+        || ctx.db.friend().hash().find(hash_b).is_some()
+    {
+        return Err("Already a friend".into());
     }
 
+    // check if the friendship was already requested
+    if ctx.db.friend_request().hash().find(hash_a).is_some()
+        || ctx.db.friend_request().hash().find(hash_b).is_some()
+    {
+        return Err("Already requested friendship".into());
+    }
+
+    // add the request to the database
+    ctx.db.friend_request().insert(FriendRequest {
+        hash: hash_a,
+        user_a: user_a.id,
+        user_b: user_b.id,
+    });
+
+    Ok(())
+}
+
+#[reducer]
+pub fn accept_friend(ctx: &ReducerContext, user_name: String) -> Result<(), String> {
+    // get users
+    let user_a = ctx.db.user().id().find(ctx.sender).ok_or("No user found")?;
+    let user_b = ctx
+        .db
+        .user()
+        .name()
+        .find(user_name)
+        .ok_or("No user found")?;
+
+    // compute hashes
+    let mut hasher = DefaultHasher::new();
+    ctx.sender.hash(&mut hasher);
+    user_b.id.hash(&mut hasher);
+    let hash_a = hasher.finish();
+
+    // we compute two hashes because the ordering changes the hash
+    let mut hasher = DefaultHasher::new();
+    user_b.id.hash(&mut hasher);
+    ctx.sender.hash(&mut hasher);
+    let hash_b = hasher.finish();
+
+    // check if the user are already friends with each other
+    if ctx.db.friend().hash().find(hash_a).is_some()
+        || ctx.db.friend().hash().find(hash_b).is_some()
+    {
+        return Err("Already a friend".into());
+    }
+
+    // compute whether to use hash_a or hash_b
+    let is_not_a = ctx.db.friend_request().hash().find(hash_a).is_none();
+    let is_not_b = ctx.db.friend_request().hash().find(hash_b).is_none();
+
+    // if we can't use either hash_a or hash_b, then there's an error because the friendship wasn't requested
+    if is_not_a || is_not_b {
+        return Err("Friendship wasn't requested".into());
+    }
+
+    // use the correct hash
+    let hash = if is_not_a { hash_b } else { hash_a };
+
+    // remove the request
+    ctx.db.friend_request().hash().delete(hash);
+    // and add as friend
     ctx.db.friend().insert(Friend {
+        hash,
         user_a: user_a.id,
         user_b: user_b.id,
     });
